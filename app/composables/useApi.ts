1b36eb988a570d63ca79e8d7e617a575a135a45c
sujitbarnawal <sujitbarnawal4545@gmail.com>
2026-01-30 11:25:54 +0000

    fix : fixed useApi composable

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -3,21 +3,20 @@ import { useAuth } from '~/composables/useAuth'
 export const useApi = () => {
   const auth = useAuth()
 
-  // Generic wrapper for $fetch
   const fetchWithAuth = async (url: string, options: any = {}) => {
     try {
       return await $fetch(url, {
         ...options,
-        credentials: 'include', // include cookies
+        credentials: 'include', 
       })
     } catch (err: any) {
-      // If 401 → token expired
+
       if (err?.response?.status === 401) {
         try {
-          // Refresh the token
+
           await auth.refreshToken()
 
-          // Retry the original request
+
           return await $fetch(url, {
             ...options,
             credentials: 'include',
